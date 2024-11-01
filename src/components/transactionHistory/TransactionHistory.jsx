fad69c69fcde20853e57eb28556065493d7a0de2
drazka <izabdr@gmail.com>
2024-11-01 15:25:14 +0100

    check classes

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -5,21 +5,21 @@ import transactions from "../../transactions.json"
 const TransactionHistory = (type, amount, currency) => {
 	return (
 
-		<table>
+		<table className={css.table}>
 		<thead>
 			<tr>
-			<th>Type</th>
-			<th>Amount</th>
-			<th>Currency</th>
+			<th className={css.th}>Type</th>
+			<th className={css.th}>Amount</th>
+			<th className={css.th}>Currency</th>
 			</tr>
 		</thead>
 
 		<tbody>
         {transactions.map(transaction => (
           <tr key={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
+            <td className={css.td}>{transaction.type}</td>
+            <td className={css.td}>{transaction.amount}</td>
+            <td className={css.td}>{transaction.currency}</td>
           </tr>
         ))}
       </tbody>
