1310ddf96f7017bc3ca87bf6f68863138d8914c6
drazka <izabdr@gmail.com>
2024-11-01 15:02:51 +0100

    task 3

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,7 +1,32 @@
-const TransactionHistory = () => {
+import clsx from "clsx";
+import css from "./TransactionHistory.module.css";
+import transactions from "../../transactions.json"
+
+const TransactionHistory = (type, amount, currency) => {
 	return (
-		<ul>
-		</ul>
+
+		<table>
+		<thead>
+			<tr>
+			<th>Type</th>
+			<th>Amount</th>
+			<th>Currency</th>
+			</tr>
+		</thead>
+
+		<tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+		</table>
+
+
+
   );
 };
 
